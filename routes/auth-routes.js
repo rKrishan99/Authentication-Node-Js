7e417b6c8da1d46f0e88691c129e3db3703b0ca0
RajithaKWG <rkrishan894@gmail.com>
2025-02-02 22:35:50 +0530

    Implemented change password

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser } = require("../controller/auth-controller");
+const { registerUser, loginUser, changePassword } = require("../controller/auth-controller");
 const router = express.Router();
+const authMiddleware = require('../middleware/auth-middleware');
 
 //all routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/change-password',authMiddleware, changePassword);
 
 module.exports = router;
