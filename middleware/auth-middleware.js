3f037f1bfbb5328f009f97674f9c3c5b1c4f4e2e
RajithaKWG <rkrishan894@gmail.com>
2025-02-02 12:17:10 +0530

    completed

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    console.log('auth middleware is called!');
+
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({
+            success: false,
+            message: 'Access denied. No token provided. Please login to contineue.'
+        })
+    }
+
+    //decode this token
+    try{
+        const decodedTokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log(decodedTokenInfo);
+        req.userInfo = decodedTokenInfo;
+
+        next();
+        
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: 'Access denied. No token provided. Please login to contineue.'
+        })
+    }
+
+    
+};
+
+module.exports = authMiddleware;
--- server.js
+++ server.js
@@ -2,17 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const connectToDB = require("./database/db");
 const authRoutes = require('./routes/auth-routes');
+const homeRoutes = require('./routes/home-routes');
+const adminRoutes = require('./routes/admin-routes');
 
 // Connect to MongoDB
 connectToDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+ 
 //middleware
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/home', homeRoutes);
+app.use('/api/admin', adminRoutes);
+
+
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
